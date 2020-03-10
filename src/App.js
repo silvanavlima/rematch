5cc443aec50d27411fc4e153ef9872ddda9e75e4
Silvana <silvana.lima@genialinvestimentos.com.br>
2020-03-10 18:31:10 -0300

    first commit

---
+++ src/App.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <input type="text" placeholder="digite a tarefa" />
+      <button>add</button>
+    </div>
+  );
+}
+
+export default App;
